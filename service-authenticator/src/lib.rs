af7237816011eb0be6f890e8bdb50c261d74891f
vitalije <vitalije@kviziracija.net>
2020-10-02 11:20:08 +0200

    doc(library): Change toplevel doc comment in src/lib.rs

--- service-authenticator/src/lib.rs
+++ service-authenticator/src/lib.rs
@@ -1,3 +1,6 @@
+//! This library is derived from yup-oauth2. Many of the doc comments are still refering
+//! to the original library.
+//! 
 //! This library can be used to acquire oauth2.0 authentication for services.
 //!
 //! For your application to use this library, you will have to obtain an application
@@ -5,67 +8,57 @@
 //! [following this guide](https://developers.google.com/youtube/registering_an_application) (for
 //! Google services) respectively the documentation of the API provider you want to connect to.
 //!
-//! # Device Flow Usage
-//! As the `DeviceFlow` involves polling, the `DeviceFlowHelper` should be used
-//! as means to adhere to the protocol, and remain resilient to all kinds of errors
-//! that can occur on the way.
-//!
 //! # Service account "flow"
 //! When using service account credentials, no user interaction is required. The access token
 //! can be obtained automatically using the private key of the client (which you can download
-//! from the API provider). See `examples/service_account/` for an example on how to use service
+//! from the API provider). See `service_account` for an example on how to use service
 //! account credentials. See
 //! [developers.google.com](https://developers.google.com/identity/protocols/OAuth2ServiceAccount)
 //! for a detailed description of the protocol. This crate implements OAuth for Service Accounts
 //! based on the Google APIs; it may or may not work with other providers.
 //!
-//! # Installed Flow Usage
-//! The installed flow involves showing a URL to the user (or opening it in a browser)
-//! and then either prompting the user to enter a displayed code, or make the authorizing
-//! website redirect to a web server spun up by this library and running on localhost.
-//!
-//! In order to use the interactive method, use the `Interactive` `InstalledFlowReturnMethod`;
-//! for the redirect method, use `HTTPRedirect`.
-//!
-//! You can implement your own `AuthenticatorDelegate` in order to customize the flow;
-//! the installed flow uses the `present_user_url` method.
-//!
 //! The returned `Token` will be stored in memory in order to authorize future
 //! API requests to the same scopes. The tokens can optionally be persisted to
 //! disk by using `persist_tokens_to_disk` when creating the authenticator.
 //!
-//! The following example, which is derived from the (actual and runnable) example in
-//! `examples/test-installed/`, shows the basics of using this crate:
+//! The following example, shows the basics of using this crate:
 //!
-//! ```test_harness,no_run
-//! use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
+//! ```
+//! use service_authenticator::parse_service_key;
+//! use service_authenticator::AuthenticatorBuilder as AB;
 //!
+//! static SERVICE_CREDENTIALS:&[u8] = include_bytes!("path to jour credentials.json");
+//! // The clientsecret file contains JSON like `{"type":"service_account", "project_id":"my-super-project", ...}`
 //! #[tokio::main]
 //! async fn main() {
-//!     // Read application secret from a file. Sometimes it's easier to compile it directly into
-//!     // the binary. The clientsecret file contains JSON like `{"installed":{"client_id": ... }}`
-//!     let secret = yup_oauth2::read_application_secret("clientsecret.json")
-//!         .await
-//!         .expect("clientsecret.json");
-//!
-//!     // Create an authenticator that uses an InstalledFlow to authenticate. The
-//!     // authentication tokens are persisted to a file named tokencache.json. The
-//!     // authenticator takes care of caching tokens to disk and refreshing tokens once
-//!     // they've expired.
-//!     let mut auth = InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::HTTPRedirect)
-//!     .persist_tokens_to_disk("tokencache.json")
-//!     .build()
-//!     .await
-//!     .unwrap();
-//!
-//!     let scopes = &["https://www.googleapis.com/auth/drive.file"];
-//!
-//!     // token(<scopes>) is the one important function of this crate; it does everything to
-//!     // obtain a token that can be sent e.g. as Bearer token.
-//!     match auth.token(scopes).await {
-//!         Ok(token) => println!("The token is {:?}", token),
-//!         Err(e) => println!("error: {:?}", e),
+//!     let service_key = parse_service_key(SERVICE_CREDENTIALS)
+//!        .expect("bad gmail credentials");
+//!     let authenticator = AB::with_service_key(service_key, ACCOUNT_EMAIL)
+//!       .build()
+//!       .await
+//!       .expect("failed to create authenticator");
+//!     // once you have authenticator, you can ask for the authorization header
+//!     // for any scopes your service account is approved
+//!     let scopes = &["https://www.googleapis.com/auth/gmail.send"];
+//!     let authorization_header = authenticator
+//!       .header(GMAIL_SCOPES)
+//!       .await
+//!       .expect("Failed to get authorization token");
+//!     // now with the authorization header you can send api requests
+//!     let mut resp = authenticator
+//!       .client
+//!       .post("https:://gmail.googleapis.com/gmail/v1/users/USEREMAIL/messages/send")
+//!       .header("Content-Type", "application/json")
+//!       .header("Authorization", authorization_header.as_str())
+//!       .send_body(r#"{"raw": "base64 encoded email message"}"#)
+//!       .await
+//!       .expect("response error");
+//!     println!("Status:{}", resp.status());
+//!     match resp.body().await {
+//!       Ok(b) => println!("Body:{:?}", &b),
+//!       Err(e) => println!("Err:{:?}", e),
 //!     }
+//!     Ok(())
 //! }
 //! ```
 //!
